473b66626fb01193f9babd336efd426c0ef954e9
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-22 17:07:18 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
-
-end
+  max_value = -1
+  count = 0
+  while count < aray.length do 
+    if max_value < array[count]
+      max_value = array[count]
+      end
+      count +=1 
+    end
+    max_value
+  end
